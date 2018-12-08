71010b86f93b65de0978c804d972815bc212981e
poop-squared <44213062+poop-squared@users.noreply.github.com>
2018-12-08 18:42:29 -0500

    fixed color bugs

--- pokemon_description.R
+++ pokemon_description.R
@@ -8,26 +8,67 @@ library("ggplot2")
 ##### Pokedex Data Description #####
 attach(pokedex)
 
-names(pokedex)[1,8,9] = c("PokeID", "Sp.Atk","Sp.Def")
+names(pokedex)[c(1,8,9)] = c("PokeID", "Sp.Atk","Sp.Def")
+names(pokedex)
 
+# Palette Color
+palette.col.long = c("#FF4933","#FF8633","#FFBB33","#FFE633","#D7FF33","#99FF33","#52FF33","#33FF86","#33FFD1",
+                "#33D4FF","#339FFF","#336BFF","#3358FF","#5233FF","#8033FF","#B233FF","#FF33C4",
+                "#FF3396")
+palette.col.short = palette.col.long[c(1,4,7,10,12,17)]
 ## Bar graph
 
 # Pokemon Type Distribution (includes legendary breakdown)
 plot1 = ggplot(pokedex, aes(Type.1))
-plot1 + geom_bar(aes(fill = Legendary)) + xlab("Principal Pokemon Type") + ylab("Number of Pokemon") + 
-  ggtitle("Pokemon Distribution by Type I") + geom_text(stat = 'count', aes(label = ..count..), vjust = -0.5) + 
+plot1 + geom_bar(aes(fill = Legendary)) + 
+  xlab("Principal Pokemon Type") +
+  ylab("Number of Pokemon") + 
+  ggtitle("Pokemon Distribution by Type I") + 
+  geom_text(stat = 'count', aes(label = ..count..), vjust = -0.5) + 
+  guides(fill=guide_legend("Legendary", nrow = 1)) +
+  scale_fill_manual(values = palette.col.short) +
   theme(legend.position = "bottom", axis.text.x = element_text(angle=90, vjust=0.5))
 
 # Pokemon Generation Distribution (includes legendary breakdown) 
 pokedex$count = 1
-Generation.Legendary = aggregate(pokedex$count, by = list(Generation = pokedex$Generation, Legendary_Pokemon = pokedex$Legendary)
+Generation.Legendary = 
+  aggregate(pokedex$count, by = list(Generation = pokedex$Generation, Legendary_Pokemon = pokedex$Legendary)
                                  , FUN = sum)
 
 'NEEDS work on the labels positioning'
 plot2 = ggplot(Generation.Legendary, aes(x = Generation, y = Generation.Legendary$x))
-plot2 + geom_bar(aes(fill = Legendary_Pokemon), stat = "identity", position = "dodge") + xlab("Generation") + 
-  ylab("Number of Pokemon") + ggtitle("Pokemon Distribution by Generation") + theme(legend.position = "bottom") +
-  geom_text(aes(label = Generation.Legendary$x), vjust=1.5, hjust=1.7,color="white", position = position_dodge(width = 0.8))
+plot2 + 
+  geom_bar(aes(fill = Legendary_Pokemon), stat = "identity", position = "dodge") +
+  xlab("Generation") + 
+  ylab("Number of Pokemon") + 
+  ggtitle("Pokemon Distribution by Generation") + 
+  theme(legend.position = "bottom") +
+  guides(fill=guide_legend("Legendary", nrow = 1)) +
+  scale_fill_manual(values = palette.col.short) +
+  geom_text(aes(label = Generation.Legendary$x), 
+            vjust=1.5, hjust=1.7,color="white", position = position_dodge(width = 0.8))
+
+Generation.Type1 = aggregate(pokedex$count, by = list(Generation = pokedex$Generation, Type = pokedex$Type.1), 
+                             FUN = sum)
+
+length(unique(Generation.Type1$Type))
+
+
+ggplot(mtcars) + 
+  geom_histogram(aes(factor(hp)), fill=getPalette(colourCount)) + 
+  theme(legend.position="right")
+
+length(Generation.Legendary$x)
+plot3 = ggplot(Generation.Type1, aes(x = Type, y = Generation.Type1$x))
+plot3 + geom_col(aes(x = Generation.Type1$Type, fill = factor(Generation.Type1$Generation), y = x)) +
+  xlab("Type") + 
+  ylab("Number of Pokemon") + 
+  ggtitle("Pokemon Distribution by Generation") + 
+  guides(fill=guide_legend("Generation", nrow = 1)) +
+  scale_fill_manual(values = palette.col.short) +
+  theme(legend.position = "bottom", axis.text.x = element_text(angle=90, vjust=0.5))
+
+
 
 #### Hierarchical clustering: divides data using a classification tree ####
 attach(pokedex)
@@ -35,7 +76,6 @@ attach(pokedex)
 ## Dendogram for Generation 1 ##
 pokedex.gen1 = pokedex[c(1:166),]
 pokedex.gen1.names = pokedex[c(1:166),]
-View(pokedex.gen1)
 rownames(pokedex.gen1.stats) = pokedex.gen1.names$Name
 
 # Create a dendogram using hierarchical clustering
@@ -53,13 +93,10 @@ hc = hclust(dist(pokedex.gen1.stats))
 
 # Real Dendogram that we will use for the paper (only generation 1 for viewing purposes)
 plot(as.phylo(hc), type = "fan")
-# cutting dendrogram in 11 clusters
+# cutting dendrogram in 18 clusters
 cluster.18 = cutree(hc, 18)
 
-palette.col = c("blueviolet","blue","beige","aquamarine","cadetblue1","skyblue","orange","darkolivegreen","red","green",
-                "yellow","plum","cyan4","wheat","magenta","dimgray","coral","goldenrod")
-#spectralcol = brewer.pal(11, "Spectral")
-plot(as.phylo(hc), type = "fan", tip.color = palette.col[cluster.18], label.offset = 1)
+plot(as.phylo(hc), type = "fan", tip.color = palette.col.long[cluster.18], label.offset = 1)
 
 # Cut the tree at 11 branches and create a vector using that clustering
 cluster.cut = cutree(hc, 18)
@@ -74,13 +111,11 @@ rownames(pokedex.allgen.stats) = pokedex$Name
 hc1 = hclust(dist(pokedex.allgen.stats))
 plot(as.phylo(hc1), type = "fan")
 cluster.all.18 = cutree(hc1, 18)
-palette.col = c("blueviolet","blue","beige","aquamarine","cadetblue1","skyblue","orange","darkolivegreen","red","green",
-                "yellow","plum","cyan4","wheat","magenta","dimgray","coral","goldenrod")
-plot(as.phylo(hc1), type = "fan", tip.color = palette.col[cluster.all.18], label.offset = 1)
+plot(as.phylo(hc1), type = "fan", tip.color = palette.col.long[cluster.all.18], label.offset = 1)
 
 # Include the new vector in the original pokedex DF
 cluster.cut.all = cutree(hc1, 18)
 pokedex$cluster = cluster.cut.all
 pokedex[order(pokedex$cluster),]
-##asd
+
 
