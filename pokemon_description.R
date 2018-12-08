19dea43636a91de22537f29f11c0a3027f3299a2
pomareda90 <pomareda.r@gmail.com>
2018-12-08 14:20:01 -0500

    Complete Part

--- pokemon_description.R
+++ pokemon_description.R
@@ -5,56 +5,73 @@ pokedex = read.csv("pokemon.csv")
 ### Libraries ###
 library("ggplot2")
 
-### Pokedex Data Description ###
+##### Pokedex Data Description #####
 attach(pokedex)
 
-str(pokedex)
-names(pokedex)
-summary(pokedex)  
-summary(pokedex$Type.1)  
-summary(pokedex$Generation)
-summary(pokedex$Legendary)
+## Bar graph
 
-table(pokedex$Legendary)
-
-# Bar graphs for Type and Generation #
-
+# Pokemon Type Distribution (includes legendary breakdown)
 plot1 = ggplot(pokedex, aes(Type.1))
 plot1 + geom_bar(aes(fill = Legendary)) + xlab("Principal Pokemon Type") + ylab("Number of Pokemon") + 
   ggtitle("Pokemon Distribution by Type") + geom_text(stat = 'count', aes(label = ..count..), vjust = -0.5) + 
   theme(legend.position = "bottom", axis.text.x = element_text(angle=90, vjust=0.5))
 
+# Pokemon Generation Distribution (includes legendary breakdown) 
 pokedex$count = 1
 Generation.Legendary = aggregate(pokedex$count, by = list(Generation = pokedex$Generation, Legendary_Pokemon = pokedex$Legendary)
                                  , FUN = sum)
-# Bar graph 
+
 'NEEDS work on the labels positioning'
 plot2 = ggplot(Generation.Legendary, aes(x = Generation, y = Generation.Legendary$x))
 plot2 + geom_bar(aes(fill = Legendary_Pokemon), stat = "identity", position = "dodge") + xlab("Generation") + 
   ylab("Number of Pokemon") + ggtitle("Pokemon Distribution by Generation") + theme(legend.position = "bottom") +
   geom_text(aes(label = Generation.Legendary$x), vjust=1.5, color="white", position = position_dodge(width = 0.8))
 
-'Not Working yet'
-library("grid")
-pushViewport(viewport(layout = grid.layout(1,2)))
-print(plot1, vp = viewport(layout.pos.row = 1, layout.pos.col = 1))
-print(plot2, vp = viewport(layout.pos.row = 1, layout.pos.col = 2))
-
-'facet_grid(~Generation)'
-
-# New graph combining pokemon main types and evolution by generation (stacked) #
+#### Hierarchical clustering: divides data using a classification tree ####
 attach(pokedex)
 
-summary(pokedex)
-pokedex$count2 = 1
-Generation.Type1 = aggregate(pokedex$count2, by = list(Generation = pokedex$Generation, Type.3 = pokedex$Type.1), 
-                             FUN = sum)
+## Dendogram for Generation 1 ##
+pokedex.gen1 = pokedex[c(1:166), -c(1,2)]
+pokedex.gen1.names = pokedex[c(1:166),]
+View(pokedex.gen1)
+rownames(pokedex.gen1) = pokedex.gen1.names$Name
 
-length(unique(Generation.Type1$Type.3))
-length(Generation.Legendary$x)
+# Create a dendogram using hierarchical clustering
+install.packages('ape')
+library("ape")
+hc = hclust(dist(pokedex.gen1))
 
-plot3 = ggplot(Generation.Type1, aes(x = Type.3, y = Generation.Type1$x))
-plot3 + geom_bar(fill = Generation.Type1$x, stat = 'identity') + xlab("Principal Pokemon Type") + ylab("Number of Pokemon") + 
-  ggtitle("Pokemon Distribution by Type") #geom_text(aes(label = ..count..), vjust = -0.5) + 
-  theme(legend.position = "bottom", axis.text.x = element_text(angle=90, vjust=0.5))
+# Plor a regular dendogram to make a "good" cut - decided to go for 11
+plot(hc, hang = -1, cex = 0.6)
+abline(h=160, col = "green", lty = 2)
+abline(h=150, col = "red", lty = 2)
+abline(h=140, col = "blue", lty = 2)
 
+# Real Dendogram that we will use for the paper (only generation 1 for viewing purposes)
+plot(as.phylo(hc), type = "fan")
+# cutting dendrogram in 11 clusters
+cluster.11 = cutree(hc, 11)
+
+library(RColorBrewer)
+spectralcol = brewer.pal(11, "Spectral")
+plot(as.phylo(hc), type = "fan", tip.color = spectralcol[cluster.11], label.offset = 1)
+
+# Cut the tree at 11 branches and create a vector using that clustering
+cluster.cut = cutree(hc, 11)
+
+# Include the new vector in the original gen1 pokedex DF
+pokedex.gen1$cluster = cluster.cut
+
+## Clustering (dendogram) for All Generations
+pokedex.allgen = pokedex[, -c(1,2)]
+rownames(pokedex.allgen) = pokedex$Name
+hc1 = hclust(dist(pokedex.allgen))
+plot(as.phylo(hc1), type = "fan")
+cluster.all.11 = cutree(hc1, 11)
+spectralcol = brewer.pal(11, "Spectral")
+plot(as.phylo(hc1), type = "fan", tip.color = spectralcol[cluster.all.11], label.offset = 1)
+# Include the new vector in the original pokedex DF
+cluster.cut.all = cutree(hc1, 11)
+pokedex$cluster = cluster.cut.all
+
+#### Principal component analysis ####
