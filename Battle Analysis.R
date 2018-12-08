6e79060917c93fa3e885367aa9364338018db047
poop-squared <44213062+poop-squared@users.noreply.github.com>
2018-12-08 14:18:53 -0500

    pca analysis on both the attributes of winners and differences in attributes

---
+++ Battle Analysis.R
@@ -0,0 +1,21 @@
+### PCA analysis ####
+
+#What are the attributes of winners
+str(all.winners)
+all.winners.vars = all.winners[c(7:12)]
+pca.winners = prcomp(all.winners.vars, scale = TRUE)
+autoplot(pca.winners, data = all.winners.vars, loadings = TRUE, loadings.label = TRUE)
+
+
+combats.diff0.vars = combats.diff0[c(2:7)]
+pca.combat.diff = prcomp(combats.diff0.vars, scale = TRUE)
+autoplot(pca.combat.diff, data = combats.diff0.vars, loadings = TRUE, loadings.label = TRUE)
+
+## good up to this point
+
+plot3d(pc$scores[,1:3])
+text3d(pc$loadings[,1:3], texts = rownames(pc$loadings), col=“red")
+        >coords=NULL
+        >for (i in 1:nrow(pc$loadings)) { coords= rbind(coords, rbind(c(0,0,0), pc$loadings[i,1:3])) }
+        > lines3d(coords, col="red", lwd=4) text3d(pc$scores[,1:3])
+        >text3d(pc$loadings[,1:3], texts=rownames(pc$loadings), col="red")
