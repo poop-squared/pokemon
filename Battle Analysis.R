8f8143f9801b8fc3a45d920a64153bb6a997fe6c
poop-squared <44213062+poop-squared@users.noreply.github.com>
2018-12-08 20:25:44 -0500

    added cluster to pokedex and sumtotal subsetted with new pokedex started % analysis

--- Battle Analysis.R
+++ Battle Analysis.R
@@ -1,3 +1,19 @@
+hp.pos = 100*nrow(combats.diff0[combats.diff0[2] > 0,])/ nrow(combats.diff0)
+atk.pos = 100*nrow(combats.diff0[combats.diff0[3] > 0,])/ nrow(combats.diff0)
+def.pos = 100*nrow(combats.diff0[combats.diff0[4] > 0,])/ nrow(combats.diff0)
+sp.atk.pos = 100*nrow(combats.diff0[combats.diff0[5] > 0,])/ nrow(combats.diff0)
+sp.def.pos = 100*nrow(combats.diff0[combats.diff0[6] > 0,])/ nrow(combats.diff0)
+speed.pos = 100*nrow(combats.diff0[combats.diff0[7] > 0,])/ nrow(combats.diff0)
+sumtotal.pos = 100*nrow(combats.diff0[combats.diff0$SumTotal > 0,])/ nrow(combats.diff0)
+
+positive.difs = c(hp.pos,atk.pos,def.pos,sp.atk.pos,sp.def.pos,speed.pos,sumtotal.pos)
+positive.difs.df = data.frame(matrix(nrow = length (positive.difs), ncol = 2))
+positive.difs.df[,2] = positive.difs
+positive.difs.df[,1] = names(combats.diff0[2:8])
+
+
+plot4+geom_bar()
+
 ### PCA analysis ####
 
 #What are the attributes of winners
