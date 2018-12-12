2fb87f47e61f5e2a58324913263a6d01d90924a9
poop-squared <44213062+poop-squared@users.noreply.github.com>
2018-12-11 23:58:45 -0500

    added code from win ratio to this document

--- Analysis_section.R
+++ Analysis_section.R
@@ -66,7 +66,54 @@ for(i in 1:nrow(res)){
              title=as.character(res$cluster[i]))
 }
 
-#### 
+#################################### Win Ratio #########################
+### perform ###
+
+##preparing data por perform analysis
+combats.cluster = merge(combats,pokedex[,c(1,14)], by.x = 'Winner', by.y = 'PokeID')
+colnames(combats.cluster)[4] = 'cluster.win'   
+
+combats.cluster = merge(combats.cluster,pokedex[,c(1,14)], by.x = 'Loser', by.y = 'PokeID')
+colnames(combats.cluster)[5] = 'cluster.lose'
+
+combats.cluster = combats.cluster[order(combats.cluster$BattleID),]
+
+
+### Printing popularity of pokemon by cluster
+
+for( i in 1:18){
+  print(nrow(subset(combats.cluster , cluster.win == i | cluster.lose == i))/nrow(combats))
+  
+}
+
+### Creating win ratio matrix by cluster  rows are winners, columns are losers
+
+total.same = rep(0,18)
+win.ratio = data.frame(matrix(nrow = 18, ncol = 18))
+
+for( i in 1:18){
+  
+  for ( j in 1:18){
+    
+    x = subset(combats.cluster , cluster.win == i & cluster.lose == j)  
+    y = subset(combats.cluster , cluster.win == j & cluster.lose == i) 
+    
+    if(i==j){
+      total.same[i] = nrow(x)
+      win.ratio[i,j] = 1
+    }else{
+      
+      win.ratio[i,j] = nrow(x)/(nrow(x)+nrow(y))
+      
+    }
+    
+  } 
+  
+}
+
+write.csv(win.ratio,'winratio.csv')
+
+
 
 ####### Test set and Training set #########################
 
@@ -362,12 +409,6 @@ test.set$error = ifelse(test.set$binom.outcome == test.set$binom.predict,0,1)
 accuracy = 100*sum(test.set$error)/nrow(test.set)
 accuracy
 
-#compare it to the actual data
-predict.df = data.frame(matrix(nrow = length(prediction), ncol = 2 ))
-names(predict.df) = c("BattleID", "first.win.prob")
-for (i in 1:length(prediction)){
-  predict$first.win.prob[1] = unname(predict(logistic.reg1, test.set2[1], type = "response"))
-}
 ############### Cluster graphs ###########################
 
 ##By Cluster
